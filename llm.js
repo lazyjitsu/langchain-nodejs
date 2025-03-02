e5e34bc5ac7a83b1f3dc876cb388c856fafa4cb7
Marko <graphneenja@gmail.com>
2025-03-02 07:43:08 -0800

    batch prompt

--- llm.js
+++ llm.js
@@ -3,6 +3,5 @@ import * as dotenv from "dotenv";
 dotenv.config();
 const model = new ChatOpenAI({})
 
-const response = await model.invoke("Hello");
+const response = await model.batch(["Hello","How are you?"]);
 console.log(response)
-
