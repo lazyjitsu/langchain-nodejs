dcc1d212d88b1080761010e633a80285a657799f
Marko <graphneenja@gmail.com>
2025-03-02 14:30:10 -0800

    basic retrieval

--- llm.js
+++ llm.js
@@ -2,7 +2,7 @@ import { ChatOpenAI } from '@langchain/openai';
 import * as dotenv from "dotenv";
 dotenv.config();
 const model = new ChatOpenAI({
-    modelName: "chatGPT-4o",
+    modelName: "gpt-4o-mini",
     temperature:0.7,
     maxTokens:1000,
     verbose: true,
--- retrieval-chain.js
+++ retrieval-chain.js
@@ -1,5 +1,7 @@
 import { ChatOpenAI } from "@langchain/openai";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { Document } from '@langchain/core/';
+import {createStuffDocumentsChain} from '@langchain/core/documents'
 
 import * as dotenv from 'dotenv';
 dotenv.config();
@@ -10,13 +12,19 @@ const model = new ChatOpenAI({
 })
 
 const prompt = ChatPromptTemplate.fromTemplate(`
-        Anser the user's question. {input}
+        Answer the user's question. 
+        Context: What is a kewl dude!
+        Question: {input}
 `)
 
 const chain = prompt.pipe(model);
 
+// Documents
+const documentA = new Document({
+    pageContent: "Marko is a Muay Thai Fighter"
+})
 const resp = await chain.invoke({
-    input:" What is Marko?",
+    input:" What is a Marko?",
 })
 
 console.log(resp);
